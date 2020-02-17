8b185ed7f6015026109da7da73a4ab4eb835bdcc
HidaKureku <61130252+HidaKureku@users.noreply.github.com>
2020-02-16 22:41:39 -0500

    Update install.py

--- install/install.py
+++ install/install.py
@@ -5,3 +5,6 @@ import os
 #Create roll_logs folder
 os.system("sudo mkdir /home/pi/catan_dice_roller/roll_logs")
 
+#Replace rc.local
+os.system("sudo rm -r /etc/rc.local")
+os.system"(sudo cp /home/pi/catan_dice_roller/install/rc.local /etc")
