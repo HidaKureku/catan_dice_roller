41ecb8714dc980ca69f224433a34ab06009927e6
HidaKureku <61130252+HidaKureku@users.noreply.github.com>
2020-02-16 22:49:43 -0500

    Update install.py

--- install/install.py
+++ install/install.py
@@ -8,3 +8,11 @@ os.system("sudo mkdir /home/pi/catan_dice_roller/roll_logs")
 #Replace rc.local
 os.system("sudo rm -r /etc/rc.local")
 os.system"(sudo cp /home/pi/catan_dice_roller/install/rc.local /etc")
+
+#Install Libraries
+os.system("sudo apt-get update -y")
+os.system("y")
+os.system("sudo apt-get upgrade -y")
+os.system("sudo apt-get install python-dev -y")
+os.system("sudo apt-get install python-dev -y")
+os.system("sudo apt-get install RPi.GPIO -y")
