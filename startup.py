5d2c9931c19545c7742633b2556a137872652f6b
HidaKureku <61130252+HidaKureku@users.noreply.github.com>
2020-02-18 21:38:07 -0500

    Update startup.py

--- startup.py
+++ startup.py
@@ -2,14 +2,17 @@ import os
 import RPi.GPIO as GPIO
 import time
 
-GPIO.setmode(GPIO.BOARD)
-
-num = [GPIO.LOW,GPIO.LOW,GPIO.LOW,GPIO.HIGH]
+eight = [GPIO.LOW,GPIO.LOW,GPIO.LOW,GPIO.HIGH]
+zero = [GPIO.LOW,GPIO.LOW,GPIO.LOW,GPIO.LOW]
 
 decoder1 = [40,38,37,36]
 decoder2 = [35,33,32,31]
 decoder3 = [29,22,18,16]
 decoder4 = [15,13,12,11]
+
+GPIO.setup(7,GPIO.OUT)
+GPIO.output(7,GPIO.LOW)
+GPIO.output(7,GPIO.HIGH)
 
 for x in range(0,4):
     GPIO.setup(decoder1[x],GPIO.OUT)
@@ -21,37 +24,29 @@ for x in range(0,4):
     GPIO.setup(decoder4[x],GPIO.OUT)
     GPIO.output(decoder4[x],GPIO.LOW)
 
-#Blank Displays
-for x in range(0,4):
-    GPIO.output(decoder1[x],blank[x])
-    GPIO.output(decoder2[x],blank[x])
-    GPIO.output(decoder3[x],blank[x])
-    GPIO.output(decoder4[x],blank[x])
-
 #Display all 8s
 for x in range(0,4):
-    GPIO.output(decoder1[x],num[8][x])
-    GPIO.output(decoder2[x],num[8][x])
-    GPIO.output(decoder3[x],num[8][x])
-    GPIO.output(decoder4[x],num[8][x])
+    GPIO.output(decoder1[x],eight[x])
+    GPIO.output(decoder2[x],eight[x])
+    GPIO.output(decoder3[x],eight[x])
+    GPIO.output(decoder4[x],eight[x])
 time.sleep(1)
 
 #Display all 0s
 for x in range(0,4):
-    GPIO.output(decoder1[x],num[0][x])
-    GPIO.output(decoder2[x],num[0][x])
-    GPIO.output(decoder3[x],num[0][x])
-    GPIO.output(decoder4[x],num[0][x])
+    GPIO.output(decoder1[x],zero[x])
+    GPIO.output(decoder2[x],zero[x])
+    GPIO.output(decoder3[x],zero[x])
+    GPIO.output(decoder4[x],zero[x])
 time.sleep(1)
 
 #Display all 8s
 for x in range(0,4):
-    GPIO.output(decoder1[x],num[8][x])
-    GPIO.output(decoder2[x],num[8][x])
-    GPIO.output(decoder3[x],num[8][x])
-    GPIO.output(decoder4[x],num[8][x])
+    GPIO.output(decoder1[x],eight[x])
+    GPIO.output(decoder2[x],eight[x])
+    GPIO.output(decoder3[x],eight[x])
+    GPIO.output(decoder4[x],eight[x])
 time.sleep(1)
 
 os.system("sudo python3 /home/pi/catan_dice_roller/catan_dice_roller.py")
-
 GPIO.cleanup()
